db4ab00a0d1ceacf93ffb808795c2c5505fcc856
maybeags <suren0709@gmail.com>
2025-01-13 20:05:56 +0900

    feat: article view uploaded

--- chapter07/src/main/java/me/ahngeunsu/springbootdeveloper/controller/BlogViewController.java
+++ chapter07/src/main/java/me/ahngeunsu/springbootdeveloper/controller/BlogViewController.java
@@ -1,11 +1,14 @@
 package me.ahngeunsu.springbootdeveloper.controller;
 
 import lombok.RequiredArgsConstructor;
+import me.ahngeunsu.springbootdeveloper.domain.Article;
 import me.ahngeunsu.springbootdeveloper.dto.ArticleListViewResponse;
+import me.ahngeunsu.springbootdeveloper.dto.ArticleViewResponse;
 import me.ahngeunsu.springbootdeveloper.service.BlogService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -24,6 +27,17 @@ public class BlogViewController {
 
         return "articleList";   // -> 우리가 이거 다음에 만들어야될 파일의 위치 및 파일명
     }
+
+    @GetMapping("/articles/{id}")
+    public String getArticle(@PathVariable Long id, Model model) {
+        Article article = blogService.findById(id);
+        model.addAttribute("article", new ArticleViewResponse(article));
+
+        return "article";
+    }
+    /*
+        templates -> article.html을 생성
+     */
 
 
 }
