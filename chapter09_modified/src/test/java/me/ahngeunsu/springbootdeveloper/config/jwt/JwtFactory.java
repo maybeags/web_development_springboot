8124f3c47c117e69dece91c1c0a425fb064eac8f
maybeags <suren0709@gmail.com>
2025-01-16 19:24:15 +0900

    feat: file uploaded

--- chapter09_modified/src/test/java/me/ahngeunsu/springbootdeveloper/config/jwt/JwtFactory.java
+++ chapter09_modified/src/test/java/me/ahngeunsu/springbootdeveloper/config/jwt/JwtFactory.java
@@ -1,5 +1,8 @@
 package me.ahngeunsu.springbootdeveloper.config.jwt;
 
+import io.jsonwebtoken.Header;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -12,7 +15,7 @@ import static java.util.Collections.emptyMap;
 @Getter
 public class JwtFactory {
     private String subject = "test@email.com";
-    private Date isuuedAt = new Date();
+    private Date issuedAt = new Date();
     private Date expiration = new Date(new Date().getTime() +
             Duration.ofDays(14).toMillis());
     private Map<String, Object> claims = emptyMap(); //import static java.util.Collections.emptyMap;
@@ -22,7 +25,7 @@ public class JwtFactory {
     public JwtFactory(String subject, Date isuuedAt,
                       Date expiration, Map<String, Object> claims) {
         this.subject = subject != null ? subject : this.subject;    // 삼항연산자
-        this.isuuedAt = isuuedAt != null ? isuuedAt : this.isuuedAt;
+        this.issuedAt = issuedAt != null ? isuuedAt : this.issuedAt;
         this.expiration = expiration != null ? expiration : this.expiration;
         this.claims = claims != null ? claims : this.claims;
     }
@@ -30,4 +33,23 @@ public class JwtFactory {
     public static JwtFactory withDefaultValues() {
         return JwtFactory.builder().build();
     }
+
+    // jjwt 라이브러리를 사용해 JWT 생성
+    public String createToken(JwtProperties jwtProperties) {
+        return Jwts.builder()
+                .setSubject(subject)
+                .setHeaderParam(Header.TYPE, Header.JWT_TYPE)
+                .setIssuer(jwtProperties.getIssuer())
+                .setIssuedAt(issuedAt)
+                .setExpiration(expiration)
+                .addClaims(claims)
+                .signWith(SignatureAlgorithm.HS256, jwtProperties.getSecretKey())
+                .compact();
+    }
 }
+/*
+    빌더 패턴을 사용해 객체를 만들 때 테스트가 필요한 데이터만 선택.
+    빌더패턴을 사용하지 않을 필드 기본값을 사용하게 됩니다.
+
+        test/java/me.../config/jwt에 TokenProviderTest.java 파일을 만들겁니다.
+ */
---
+++ chapter09_modified/src/test/java/me/ahngeunsu/springbootdeveloper/config/jwt/TokenProviderTest.java
@@ -0,0 +1,7 @@
+package me.ahngeunsu.springbootdeveloper.config.jwt;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TokenProviderTest {
+
+}
