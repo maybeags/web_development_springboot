2d30e4ee427519855707bd5c449831408e1d7dae
maybeags <suren0709@gmail.com>
2025-01-17 19:24:32 +0900

    feat: ApiControllerTest uploaded

--- chapter09_modified/src/test/java/me/ahngeunsu/springbootdeveloper/controller/TokenApiControllerTest.java
+++ chapter09_modified/src/test/java/me/ahngeunsu/springbootdeveloper/controller/TokenApiControllerTest.java
@@ -1,7 +1,82 @@
 package me.ahngeunsu.springbootdeveloper.controller;
+/*
+    Given
+        - 테스트 유저를 생성, jjwt 라이브러리 이용해서 리프레시 토큰을 만들어
+        DB에 저장, 토큰 생성 API의 요청 본문에 토큰을 포함하여 요청 객체를 생성
 
-import static org.junit.jupiter.api.Assertions.*;
+    When
+        - 토큰 추가 API에 요청을 보냅니다. 이때 요청 타입은 JSON이며,
+        given 절에서 미리 만들어둔 객체를 요청 본문으로 함께 보냅니다.
 
+    Then
+        - 응답 코드가 201 Created인지 확인하고, 응답으로 온 액세스 토큰이
+        비어있지 않은지 확인
+ */
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import me.ahngeunsu.springbootdeveloper.config.jwt.JwtFactory;
+import me.ahngeunsu.springbootdeveloper.config.jwt.JwtProperties;
+import me.ahngeunsu.springbootdeveloper.domain.RefreshToken;
+import me.ahngeunsu.springbootdeveloper.domain.User;
+import me.ahngeunsu.springbootdeveloper.dto.CreateAccessTokenRequest;
+import me.ahngeunsu.springbootdeveloper.repository.RefreshTokenRepository;
+import me.ahngeunsu.springbootdeveloper.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.Map;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 class TokenApiControllerTest {
+    @Autowired
+    protected MockMvc mockMvc;
+    @Autowired
+    protected ObjectMapper objectMapper;
+    @Autowired
+    private WebApplicationContext context;
+    @Autowired
+    JwtProperties jwtProperties;
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    RefreshTokenRepository refreshTokenRepository;
+
+    @BeforeEach
+    public void mockMvcSetUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
+                .build();
+        userRepository.deleteAll();
+    }
+
+    @DisplayName("createNewAccessToken() : 새로운 액세스 토큰을 발급")
+    @Test
+    public void createNewAccessToken() throws Exception {
+        // given
+        final String url = "/api/token";
+
+        User testUser = userRepository.save(User.builder()
+                .email("user@gmail.com")
+                .password("text")
+                .build());
+
+        String refreshToken = JwtFactory.builder()
+                .claims(Map.of("id", testUser.getId()))
+                .build().createToken(jwtProperties);
+
+        refreshTokenRepository.save(new RefreshToken(testUser.getId(), refreshToken));
+
+        CreateAccessTokenRequest request = new CreateAccessTokenRequest();
+        request.setRefreshToken(refreshToken);
+        final String requestBody = objectMapper.writeValueAsString(request);
+    }
+
 
 }
