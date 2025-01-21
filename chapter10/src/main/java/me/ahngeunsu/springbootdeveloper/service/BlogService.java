4ae7ed23f852902e95959e1f8c77f6de7aec3666
maybeags <suren0709@gmail.com>
2025-01-21 20:16:29 +0900

    feat: fixed version

--- chapter10/src/main/java/me/ahngeunsu/springbootdeveloper/service/BlogService.java
+++ chapter10/src/main/java/me/ahngeunsu/springbootdeveloper/service/BlogService.java
@@ -6,6 +6,7 @@ import me.ahngeunsu.springbootdeveloper.domain.Article;
 import me.ahngeunsu.springbootdeveloper.dto.AddArticleRequest;
 import me.ahngeunsu.springbootdeveloper.dto.UpdateArticleRequest;
 import me.ahngeunsu.springbootdeveloper.repository.BlogRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -31,8 +32,26 @@ public class BlogService {
                 .orElseThrow(() -> new IllegalArgumentException("not found: " + id));   // 람다식
     }
 
+//    public void delete(long id) {
+//        blogRepository.deleteById(id);
+//    }
+//
+//    @Transactional
+//    public Article update(long id, UpdateArticleRequest request) {
+//        Article article = blogRepository.findById(id)
+//                .orElseThrow(() -> new IllegalArgumentException("not found : " + id));
+//
+//        article.update(request.getTitle(), request.getContent());
+//
+//        return article;
+//    }
+
     public void delete(long id) {
-        blogRepository.deleteById(id);
+        Article article = blogRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("not found : " + id));
+
+        authorizeArticleAuthor(article);
+        blogRepository.delete(article);
     }
 
     @Transactional
@@ -40,9 +59,17 @@ public class BlogService {
         Article article = blogRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("not found : " + id));
 
+        authorizeArticleAuthor(article);
         article.update(request.getTitle(), request.getContent());
 
         return article;
+    }
+
+    private void authorizeArticleAuthor(Article article) {
+        String userName = SecurityContextHolder.getContext().getAuthentication().getName();
+        if (!article.getAuthor().equals(userName)) {
+            throw new IllegalArgumentException("not authorized");
+        }
     }
 
 }
